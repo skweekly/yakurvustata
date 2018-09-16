3fdc0cbe0884ba8e551d0007566da911ec61aabe
skweekly <41743710+skweekly@users.noreply.github.com>
2018-09-16 22:42:35 +0300

    Implementing a statistics feature

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+//declaring shit needed for later
 const TwitchBot = require('twitch-bot')
+const fs = require('fs')
 let pillNum = 77522
 let pillSteal = Math.floor(pillNum)
 let currentPills
-
+let statistics = {
+    wins: 0
+    losses: 0
+    joins: 0
+};
+    
 const Bot = new TwitchBot({
     username : 'berdygaming',
     oauth    : 'oauth:9chop2wdusouwz4webw2vg6uv3nl0z',
@@ -24,6 +31,9 @@ Bot.on("message", chatter => {
     setTimeout(function(){
       Bot.say("!runaway")
       console.log("You participated in a !runaway with " + pillSteal + " at " +  new Date().toLocaleString())
+        statistcs.joins++
+        let data = JSON.stringify(statistcs.joins, null, 2);
+        fs.writeFileSync('stats.json', data); 
     }, 12000
    )
 
@@ -36,6 +46,9 @@ Bot.on("message", chatter => {
     setTimeout(function(){
       Bot.say("!steal " + Math.floor(pillSteal))
       console.log("You participated in a !steal with " + pillSteal + " at " +  new Date().toLocaleString())
+          statistcs.joins++
+        let data = JSON.stringify(statistcs.joins, null, 2);
+        fs.writeFileSync('stats.json', data); 
     }, 12000
    )
 
@@ -58,6 +71,9 @@ Bot.on("message", chatter => {
   if(chatter.message.includes("Here's what happened...") && chatter.message.includes("berdygaming")) {
     pillSteal = pillSteal + (pillSteal / 2)
     console.log("You won the !steal with " + pillSteal + " at " +  new Date().toLocaleString())
+        statistcs.wins++
+        let data = JSON.stringify(statistcs.wins, null, 2);
+        fs.writeFileSync('stats.json', data); 
   }
   })
 
@@ -65,19 +81,27 @@ Bot.on("message", chatter => {
   if(chatter.message.includes("Here's what happened...") && !chatter.message.includes("berdygaming")){
       pillSteal = Math.floor(pillSteal - (pillSteal / 2))
       console.log("You lost the !steal with " + pillSteal + " at " +  new Date().toLocaleString())
+       statistcs.losses++
+        let data = JSON.stringify(statistcs.losses, null, 2);
+        fs.writeFileSync('stats.json', data); 
   }
 })
 
 Bot.on("message", chatter => {
   if(chatter.message.includes(" It's over. There are pills everywhere.") && chatter.message.includes("berdygaming")){
       console.log("You won the !runaway with " + pillSteal + " at " +  new Date().toLocaleString())
-    
+     statistcs.wins++
+        let data = JSON.stringify(statistcs.joins, null, 2);
+        fs.writeFileSync('stats.json', data); 
   }
 })
 
 Bot.on("message", chatter => {
   if(chatter.message.includes(" It's over. There are pills everywhere.") && !chatter.message.includes("berdygaming")){
       console.log("You lost the !runaway with " + pillSteal + " at " +  new Date().toLocaleString())
+       statistcs.losses++
+        let data = JSON.stringify(statistcs.losses, null, 2);
+        fs.writeFileSync('stats.json', data); 
   }
 })
 
@@ -90,5 +114,8 @@ Bot.on("message", chatter => {
 Bot.on("message", chatter => {
      if(chatter.message.includes("Sad and alone, berdygaming never stood a chance")){
         console.log("You lost the !runaway with " + pillSteal + " at " +  new Date().toLocaleString()) 
+          statistcs.losses++
+        let data = JSON.stringify(statistcs.losses, null, 2);
+        fs.writeFileSync('stats.json', data); 
   }
 })
